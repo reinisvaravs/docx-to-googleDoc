912a86780d277b74c1bb4cf3fc52d8c93871c001
reinisvaravs <reinisroberts.varavs2@gmail.com>
2025-08-06 04:57:44 +0300

    fix

--- routes/n8n.js
+++ routes/n8n.js
@@ -218,11 +218,10 @@ router.post("/get_message_by_id", async (req, res) => {
 
   for (let i = 1; i < output.length; i++) {
     if (output[output.length - i].message_id === message_id) {
-      res.status(200).json(output[output.length - i].content);
-      break;
+      return res.status(200).json(output[output.length - i].content);
     }
   }
-  res.status(200).json({ content: "Message not found" });
+  return res.status(200).json({ content: "Message not found" });
 });
 
 export default router;
